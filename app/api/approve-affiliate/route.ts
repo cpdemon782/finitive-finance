51e34295d68395b36ac8c0f139d9e53af39ab464
cpdemon782 <simon@clesandco.com.au>
2026-07-05 21:58:56 +1000

    Update route.ts

--- app/api/approve-affiliate/route.ts
+++ app/api/approve-affiliate/route.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
-import { welcomeEmail } from '../../lib/emails'
+import { welcomeEmail } from '../../../lib/emails'
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
